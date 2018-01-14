3ba292b91202781ba72aec10cf27d4342eb7d960
Luka Nedejlkovic <35425476+Aphace@users.noreply.github.com>
2018-01-14 20:49:31 +0600

    i Added The files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,290 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ThirteenForm1 = New WindowsApp1.ThirteenForm()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.RichTextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.ThirteenButton1 = New WindowsApp1.ThirteenButton()
+        Me.ThirteenControlBox1 = New WindowsApp1.ThirteenControlBox()
+        Me.ThirteenForm1.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ThirteenForm1
+        '
+        Me.ThirteenForm1.AccentColor = System.Drawing.Color.DodgerBlue
+        Me.ThirteenForm1.BackColor = System.Drawing.Color.FromArgb(CType(CType(50, Byte), Integer), CType(CType(50, Byte), Integer), CType(CType(50, Byte), Integer))
+        Me.ThirteenForm1.ColorScheme = WindowsApp1.ThirteenForm.ColorSchemes.Dark
+        Me.ThirteenForm1.Controls.Add(Me.GroupBox1)
+        Me.ThirteenForm1.Controls.Add(Me.ThirteenControlBox1)
+        Me.ThirteenForm1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ThirteenForm1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!)
+        Me.ThirteenForm1.ForeColor = System.Drawing.Color.White
+        Me.ThirteenForm1.Location = New System.Drawing.Point(0, 0)
+        Me.ThirteenForm1.Name = "ThirteenForm1"
+        Me.ThirteenForm1.Size = New System.Drawing.Size(951, 497)
+        Me.ThirteenForm1.TabIndex = 0
+        Me.ThirteenForm1.Text = "Ax0nes Email Spammer"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(50, Byte), Integer), CType(CType(50, Byte), Integer), CType(CType(52, Byte), Integer))
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.GroupBox2)
+        Me.GroupBox1.ForeColor = System.Drawing.SystemColors.ButtonShadow
+        Me.GroupBox1.Location = New System.Drawing.Point(62, 58)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(841, 398)
+        Me.GroupBox1.TabIndex = 2
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Main Hub"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Label4.Location = New System.Drawing.Point(148, 379)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(575, 16)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Node: This can be used for Phishing, and the owner of the tool is not responsible" &
+    " off your actions!"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Label3.Location = New System.Drawing.Point(35, 27)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(443, 16)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "NOTE: When The Button ""Send"" is clicked the email will send in 1 - 5 mins."
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Label2.Location = New System.Drawing.Point(768, 379)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(67, 16)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "ZyperSec"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Label1.Location = New System.Drawing.Point(6, 379)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(77, 16)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Version: 1.0"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Label7)
+        Me.GroupBox2.Controls.Add(Me.TextBox6)
+        Me.GroupBox2.Controls.Add(Me.Label8)
+        Me.GroupBox2.Controls.Add(Me.TextBox1)
+        Me.GroupBox2.Controls.Add(Me.Label9)
+        Me.GroupBox2.Controls.Add(Me.TextBox4)
+        Me.GroupBox2.Controls.Add(Me.Label6)
+        Me.GroupBox2.Controls.Add(Me.TextBox3)
+        Me.GroupBox2.Controls.Add(Me.Label5)
+        Me.GroupBox2.Controls.Add(Me.TextBox2)
+        Me.GroupBox2.Controls.Add(Me.ThirteenButton1)
+        Me.GroupBox2.ForeColor = System.Drawing.SystemColors.AppWorkspace
+        Me.GroupBox2.Location = New System.Drawing.Point(38, 55)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(764, 310)
+        Me.GroupBox2.TabIndex = 2
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Hub"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(149, 149)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(53, 16)
+        Me.Label7.TabIndex = 15
+        Me.Label7.Text = "Amount"
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(238, 143)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.ReadOnly = True
+        Me.TextBox6.Size = New System.Drawing.Size(275, 22)
+        Me.TextBox6.TabIndex = 14
+        Me.TextBox6.Text = "1"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(149, 27)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(51, 16)
+        Me.Label8.TabIndex = 13
+        Me.Label8.Text = "Target:"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(238, 21)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(275, 22)
+        Me.TextBox1.TabIndex = 12
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(149, 199)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(65, 16)
+        Me.Label9.TabIndex = 11
+        Me.Label9.Text = "Message"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(238, 196)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(275, 99)
+        Me.TextBox4.TabIndex = 10
+        Me.TextBox4.Text = ""
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(149, 109)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(52, 16)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Sender"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(238, 103)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(275, 22)
+        Me.TextBox3.TabIndex = 4
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(149, 67)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(53, 16)
+        Me.Label5.TabIndex = 3
+        Me.Label5.Text = "Subject"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(238, 61)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(275, 22)
+        Me.TextBox2.TabIndex = 2
+        '
+        'ThirteenButton1
+        '
+        Me.ThirteenButton1.AccentColor = System.Drawing.Color.DodgerBlue
+        Me.ThirteenButton1.BackColor = System.Drawing.Color.FromArgb(CType(CType(50, Byte), Integer), CType(CType(50, Byte), Integer), CType(CType(50, Byte), Integer))
+        Me.ThirteenButton1.ColorScheme = WindowsApp1.ThirteenButton.ColorSchemes.Dark
+        Me.ThirteenButton1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!)
+        Me.ThirteenButton1.ForeColor = System.Drawing.Color.White
+        Me.ThirteenButton1.Location = New System.Drawing.Point(573, 225)
+        Me.ThirteenButton1.Name = "ThirteenButton1"
+        Me.ThirteenButton1.Size = New System.Drawing.Size(146, 49)
+        Me.ThirteenButton1.TabIndex = 1
+        Me.ThirteenButton1.Text = "Send"
+        Me.ThirteenButton1.UseVisualStyleBackColor = False
+        '
+        'ThirteenControlBox1
+        '
+        Me.ThirteenControlBox1.AccentColor = System.Drawing.Color.DodgerBlue
+        Me.ThirteenControlBox1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ThirteenControlBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(50, Byte), Integer), CType(CType(50, Byte), Integer), CType(CType(50, Byte), Integer))
+        Me.ThirteenControlBox1.ColorScheme = WindowsApp1.ThirteenControlBox.ColorSchemes.Dark
+        Me.ThirteenControlBox1.ForeColor = System.Drawing.Color.White
+        Me.ThirteenControlBox1.Location = New System.Drawing.Point(847, 1)
+        Me.ThirteenControlBox1.Name = "ThirteenControlBox1"
+        Me.ThirteenControlBox1.Size = New System.Drawing.Size(100, 25)
+        Me.ThirteenControlBox1.TabIndex = 0
+        Me.ThirteenControlBox1.Text = "ThirteenControlBox1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(951, 497)
+        Me.Controls.Add(Me.ThirteenForm1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.Text = "Email Spoofer"
+        Me.ThirteenForm1.ResumeLayout(False)
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents ThirteenForm1 As ThirteenForm
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents ThirteenButton1 As ThirteenButton
+    Friend WithEvents ThirteenControlBox1 As ThirteenControlBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents TextBox4 As RichTextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents TextBox6 As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,24 @@
+﻿Public Class Form1
+    Dim wclient As New System.Net.WebClient
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+
+    End Sub
+
+    Private Sub GroupBox2_Enter(sender As Object, e As EventArgs) Handles GroupBox2.Enter
+
+    End Sub
+
+    Private Sub ThirteenButton1_Click(sender As Object, e As EventArgs) Handles ThirteenButton1.Click
+
+
+        Dim result As String = wclient.DownloadString("http://cheatapp.us/test/coc2/img/cf/email.php?target=" + TextBox1.Text + "&subject=" + TextBox2.Text + "&message=" + TextBox4.Text + "&amount=" + TextBox6.Text + "&sender=" + TextBox3.Text)
+
+        MessageBox.Show("The spoofed mail have been sent!")
+
+    End Sub
+End Class
